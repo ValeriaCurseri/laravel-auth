4a3891f43851a954f21418193ac15129b8b2decd
Valeria Curseri <curserivaleria@gmail.com>
2020-11-08 13:37:32 +0100

    Aggiustamenti bottoni

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -11,7 +11,7 @@
     @if(!empty($post['img']))
         <img src="{{ asset('storage/' . $post['img']) }}" class="img-fluid" alt="Responsive image">
     @endif
-    <p>{{ $post['articolo'] }}</p>
+    <p class="mt-5">{{ $post['articolo'] }}</p>
     <small class="d-block mb-5">Pubblicato da {{ $nomeUtente }} {{ Carbon\Carbon::parse($post->updated_at)->diffForHumans()}}</small>
     <small class="d-block mb-5 text-muted"><strong>Tag</strong> 
         @foreach($tags as $tag)
@@ -19,11 +19,13 @@
         @endforeach
     </small>
     
-    <form class="d-inline mr-5" action="{{ route('admin.posts.destroy', $post->id) }}" method="post">
-        @csrf
-        @method('DELETE')
-        <input class="btn btn-dark mb-5" type="submit" value="Cancella articolo">
-    </form>
-    <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-dark mb-5">Modifica l'articolo</a>
+    @if(Auth::check())
+        <form class="d-inline mr-5" action="{{ route('admin.posts.destroy', $post->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input class="btn btn-dark mb-5" type="submit" value="Cancella articolo">
+        </form>
+        <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-dark mb-5">Modifica l'articolo</a>
+    @endif
 </div>
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,7 +6,7 @@
     @if(!empty($post['img']))
         <img src="{{ asset('storage/' . $post['img']) }}" class="img-fluid" alt="{{ $post->titolo }}">
     @endif
-    <p>{{ $post['articolo'] }}</p>
+    <p class="mt-5">{{ $post['articolo'] }}</p>
     <small class="d-block mb-5">Pubblicato da {{ $nomeUtente }}</small>
     <small class="d-block mb-5 text-muted"><strong>Tag</strong> 
         @foreach($post->tags as $tag)
@@ -14,11 +14,13 @@
         @endforeach
     </small>
     
-    <form class="d-inline mr-5" action="{{ route('admin.posts.destroy', $post->id) }}" method="post">
-        @csrf
-        @method('DELETE')
-        <input class="btn btn-dark mb-5" type="submit" value="Cancella articolo">
-    </form>
-    <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-dark mb-5">Modifica l'articolo</a>
+    @if(Auth::check())
+        <form class="d-inline mr-5" action="{{ route('admin.posts.destroy', $post->id) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <input class="btn btn-dark mb-5" type="submit" value="Cancella articolo">
+        </form>
+        <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-dark mb-5">Modifica l'articolo</a>
+    @endif
 </div>
 @endsection
